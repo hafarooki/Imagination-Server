3a123d6e7ee5318d5cb1248ae8758bff0c053578
Hameed Farooki <miclebrick@outlook.com>
2015-09-03 19:26:52 -0400

    Added missing files

---
+++ ImaginationServer.Auth.Packets/ImaginationServer.Auth.Packets.cpp
@@ -0,0 +1,72 @@
+// This is the main DLL file.
+
+#include "stdafx.h"
+
+#include "ImaginationServer.Auth.Packets.h"
+#include "LoginResponse.h"
+#include <RakPeerInterface.h>
+#include <BitStream.h>
+#include <RakNetworkFactory.h>
+#include <MessageIdentifiers.h>
+
+using namespace ImaginationServer::Common;
+using namespace RakNet;
+
+void ImaginationServerAuthPackets::AuthPackets::SendLoginResponse(String ^ address, unsigned char success, String^ userKey)
+{
+	BitStream bitStream;
+	CreatePacketHeader(ID_USER_PACKET_ENUM, 5, 0, &bitStream);
+	LoginResponse response(success, userKey);
+	bitStream.Write(response.LoginStatus);
+	WriteStringToBitStream(response.TalkLikeAPirate.c_str(), 18, 33, &bitStream);
+	for (int i = 0; i < 7; i++) {
+		WriteStringToBitStream(response.UnknownString.c_str(), 0, 33, &bitStream);
+	}
+	bitStream.Write(response.ClientVersion1);
+	bitStream.Write(response.ClientVersion2);
+	bitStream.Write(response.ClientVersion3);
+	WriteStringToBitStream(response.UserKey.c_str(), 66, 66, &bitStream);
+	WriteStringToBitStream(response.RedirectIp.c_str(), sizeof(response.RedirectIp), 33, &bitStream);
+	WriteStringToBitStream(response.ChatIp.c_str(), sizeof(response.ChatIp), 33, &bitStream);
+	bitStream.Write(response.RedirectPort);
+	bitStream.Write(response.ChatPort);
+	WriteStringToBitStream(response.AnotherIp.c_str(), sizeof(response.AnotherIp), 33, &bitStream);
+	WriteStringToBitStream(response.PossibleGuid.c_str(), 37, 37, &bitStream);
+	bitStream.Write(response.ZeroShort);
+	bitStream.Write(response.LocalizationChar);
+	bitStream.Write(response.FirstLoginSubscription);
+	bitStream.Write(response.Subscribed);
+	bitStream.Write(response.ZeroLong);
+	bitStream.Write(response.ErrorMsgLength);
+	WriteStringToBitStream(response.ErrorMsg.c_str(), response.ErrorMsgLength, 1, &bitStream);
+	bitStream.Write(response.ExtraBytesLength);
+
+	CreateExtraPacketData(0, 0, 2803442767, &bitStream);
+	CreateExtraPacketData(7, 37381, 2803442767, &bitStream);
+	CreateExtraPacketData(8, 6, 2803442767, &bitStream);
+	CreateExtraPacketData(9, 0, 2803442767, &bitStream);
+	CreateExtraPacketData(10, 0, 2803442767, &bitStream);
+	CreateExtraPacketData(11, 1, 2803442767, &bitStream);
+	CreateExtraPacketData(14, 1, 2803442767, &bitStream);
+	CreateExtraPacketData(15, 0, 2803442767, &bitStream);
+	CreateExtraPacketData(17, 1, 2803442767, &bitStream);
+	CreateExtraPacketData(5, 0, 2803442767, &bitStream);
+	CreateExtraPacketData(6, 1, 2803442767, &bitStream);
+	CreateExtraPacketData(20, 1, 2803442767, &bitStream);
+	CreateExtraPacketData(19, 30854, 2803442767, &bitStream);
+	CreateExtraPacketData(21, 0, 2803442767, &bitStream);
+	CreateExtraPacketData(22, 0, 2803442767, &bitStream);
+	CreateExtraPacketData(23, 4114, 2803442767, &bitStream);
+	CreateExtraPacketData(27, 4114, 2803442767, &bitStream);
+	CreateExtraPacketData(28, 1, 2803442767, &bitStream);
+	CreateExtraPacketData(29, 0, 2803442767, &bitStream);
+	CreateExtraPacketData(30, 30854, 2803442767, &bitStream);
+
+	auto server = LuServer::CurrentServer;
+	auto split = address->Split(':');
+	SystemAddress unmanaged;
+	unmanaged.SetBinaryAddress((char*)(System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(split[0])).ToPointer());
+	unmanaged.port = (unsigned short)strtoul((char*)(System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(split[1])).ToPointer(), NULL, 0);
+	server->GetPeer()->Send(&bitStream, SYSTEM_PRIORITY, RELIABLE_ORDERED, 0, unmanaged, false);
+}
+
---
+++ ImaginationServer.Auth.Packets/ImaginationServer.Auth.Packets.h
@@ -0,0 +1,55 @@
+// ImaginationServer.Auth.Packets.h
+
+#pragma once
+
+#include "Stdafx.h"
+#include <BitStream.h>
+
+using namespace System;
+using namespace RakNet;
+
+namespace ImaginationServerAuthPackets {
+
+	public ref class AuthPackets
+	{
+		static void CreateExtraPacketData(unsigned long stampId, signed long bracketNum, unsigned long afterNum, RakNet::BitStream *bitStream) {
+			unsigned long zeroPacket = 0;
+
+			bitStream->Write(stampId);
+			bitStream->Write(bracketNum);
+			bitStream->Write(afterNum);
+			bitStream->Write(zeroPacket);
+		}
+	public:
+		static void SendLoginResponse(String^ address, unsigned char success, String^ userkey);
+
+		static void WriteStringToBitStream(const char* myString, int stringSize, int maxChars, RakNet::BitStream* output)
+		{
+			// Write the string to provided BitStream along with the size
+			output->Write(myString, stringSize);
+
+			// Check to see if there are any bytes remaining according to user
+			// specification
+			auto remaining = maxChars - stringSize;
+
+			// If so, fill with 0x00
+			for (auto i = 0; i < remaining; i++)
+			{
+				unsigned char zero = 0;
+
+				output->Write(zero);
+			}
+		}
+
+		static void CreatePacketHeader(MessageID messageId, unsigned short connectionType, unsigned long internalPacketId, BitStream* output)
+		{
+			unsigned char unknown1 = 0; // This is an unknown uchar
+
+										// Write data to provided BitStream
+			output->Write(messageId);
+			output->Write(connectionType);
+			output->Write(internalPacketId);
+			output->Write(unknown1);
+		}
+	};
+}
