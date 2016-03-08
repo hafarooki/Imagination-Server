50ec271879a8a93b1c3b9dc247c321b3c71651f7
Hameed Farooki <miclebrick@outlook.com>
2016-03-08 16:21:23 -0500

    Added untracked files

---
+++ ImaginationServer.Common/Data/BackpackItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ImaginationServer.Common.Data
+{
+    public class BackpackItem
+    {
+        public virtual int Id { get; set; }
+        public virtual long Lot { get; set; }
+        public virtual int Slot { get; set; }
+        public virtual int Count { get; set; }
+        public virtual bool Linked { get; set; }
+    }
+}
